7c957265a9f16ec741e68d14a53e9118d2042578
lixiang <675196450@qq.com>
2020-12-04 12:19:08 +0800

    增加leakcanary

--- app/src/main/java/com/medi/androidxdevelop/base/BaseApplication.kt
+++ app/src/main/java/com/medi/androidxdevelop/base/BaseApplication.kt
@@ -5,26 +5,53 @@ import android.util.Log
 import android.widget.Toast
 import com.github.moduth.blockcanary.BlockCanary
 import com.github.moduth.blockcanary.BlockCanaryContext
+import com.medi.androidxdevelop.leakcanary.LeakUploader
 import com.meituan.android.walle.WalleChannelReader
 import com.sensorsdata.analytics.android.sdk.SensorsDataAPI
+import leakcanary.AppWatcher
+import leakcanary.LeakCanary
+import okhttp3.internal.wait
 
 /**
  * Created by lixiang on 2020/4/1
  * Describe:
  */
-class BaseApplication: Application(){
+class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         ApplicationContext.application = this
         ApplicationContext.context = this
 
         val channel = WalleChannelReader.getChannel(this.applicationContext)
-        Toast.makeText(this,"channel==${channel}",Toast.LENGTH_SHORT).show()
-        Log.d("BaseApplication","channel==${channel}")
+        Toast.makeText(this, "channel==${channel}", Toast.LENGTH_SHORT).show()
+        Log.d("BaseApplication", "channel==${channel}")
         //ASM
         initASMSensorsDataAPI(this)
         //BlockCanary
         BlockCanary.install(this, BlockCanaryContext()).start()
+        initLeakCanary()
+    }
+
+    /**
+     * 2.0版本之后不需要再进行配置
+     */
+    private fun initLeakCanary() {
+        //配置LeakCanary
+       /* if (LeakCanary.isInAnalyzerProcess(this)) {
+            return
+        }
+        LeakCanary.install(this)*/
+
+
+        //您可以观看不再需要的任何对象，例如分离视图或损坏的演示者：
+        //AppWatcher.objectWatcher.watch()
+
+        //禁用LeakCanary
+        //LeakCanary.config.copy(dumpHeap = false)//禁用堆转储和分析
+        //LeakCanary.showLeakDisplayActivityLauncherIcon(false)//隐藏泄漏显示活动启动器图标
+
+        //配置上传到服务器的堆转储
+        //LeakCanary.config = LeakCanary.config.copy(onHeapAnalyzedListener = LeakUploader())
     }
 
     /**
@@ -32,7 +59,9 @@ class BaseApplication: Application(){
      *
      * @param application Application
      */
-    private  fun initASMSensorsDataAPI(application: Application) {
+    private fun initASMSensorsDataAPI(application: Application) {
         SensorsDataAPI.init(application)
     }
+
+
 }
--- readme_conclusion.md
+++ readme_conclusion.md
@@ -1,10 +1,10 @@
 1.MVC、MVP、MVVM
   MVC：标准已经多样化，无需纠结
 专业技能：
-熟悉TCP/IP、HTTP、HTTPS网络通讯协议.了解TCP/IP的四层结构.对一些常用header以及状态码有过了解。
-熟悉Android的消息机制,明白同步屏障的实现原理和使用场景,了解过IdleHandler的处理流程
+熟悉TCP/IP、HTTP、HTTPS网络通讯协议，对一些常用header以及状态码有过了解。
+熟悉Android的消息机制,明白同步屏障的实现原理和使用场景,了解IdleHandler的处理流程
 熟练掌握View的事件分发,处理过嵌套View的滑动相关问题
-对自定义View,有较多的开发经验,熟练掌握View的绘制流程,也能写出较为高效的代码.
+熟练掌握自定义View,,了解View的刷新机制和绘制流程，了解Choreographer的处理流程.
 熟悉多线程通信,了解一些常用锁概念,如悲观锁、乐观锁、自旋锁等.也了解过Java虚拟机对锁的优化
 
 项目技能：
